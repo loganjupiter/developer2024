ddf42a00475e7d1b34f6acc082e75b24185379c0
Logan Aresche <logan.aresche@anima.edu.uy>
2024-03-22 21:08:00 -0300

    excersise 8 Logan
    Tengo que arreglar el index de episodes

--- exercise8-modularizado - LOGAN/routes/charRoutes copy.js
+++ exercise8-modularizado - LOGAN/routes/charRoutes copy.js
@@ -4,22 +4,22 @@ const router = express.Router();
 //router.get
 router.get("/chars", async (req, res) => {
   try {
-    const respuesta = await fetch("https://rickandmortyapi.com/api/episode");
-    const episode = await respuesta.json();
-    res.send(episode);
+    const respuesta = await fetch("https://rickandmortyapi.com/api/character");
+    const personajes = await respuesta.json();
+    res.send(personajes);
   } catch (error) {
     console.log("Ocurrió un error en el fetch", error);
   }
 });
 
-router.get("/episode/:id", async (req, res) => {
+router.get("/chars/:id", async (req, res) => {
   const id = req.params.id;
   try {
-    const respuest = await fetch(
-      `https://rickandmortyapi.com/api/episode/${id}.`
+    const respuesta = await fetch(
+      `https://rickandmortyapi.com/api/character/${id}.`
     );
-    const episodes = await respuest.json();
-    res.send(episodes);
+    const personaje = await respuesta.json();
+    res.send(personaje);
   } catch (error) {
     console.log("Ocurrió un error en el fetch", error);
   }
